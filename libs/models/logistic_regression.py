4f3375c66ac16cc0039c23b4be6ae5ceae4814a4
JacopoCaldana <jcaldana.jc@gmail.com>
2024-11-28 12:31:38 +0100

    Update comments logistic_regression.py

--- libs/models/logistic_regression.py
+++ libs/models/logistic_regression.py
@@ -9,67 +9,36 @@ class LogisticRegression:
         self.parameters = np.random.normal(0, 0.01, num_features)
         
     def predict(self, x:np.array) -> np.array:
-        """
-        Method to compute the predictions for the input features.
-
-        Args:
-            x: it's the input data matrix.
-
-        Returns:
-            preds: the predictions of the input features.
-        """
+        
         ##############################
+        #We take the dot product of input features x with the  model parameters and then we compute the sigmoid activation on it.
         preds = sigmoid(np.dot(x, self.parameters))
         ##############################
         return preds
     
     @staticmethod
     def likelihood(preds, y : np.array) -> np.array:
-        """
-        Function to compute the log likelihood of the model parameters according to data x and label y.
-
-        Args:
-            preds: the predicted labels.
-            y: the label array.
-
-        Returns:
-            log_l: the log likelihood of the model parameters according to data x and label y.
-        """
+        
         ##############################
+        #We calculate the binary cross-entropy loss with numerical stability adding a small constant.
         log_l = np.mean(y * np.log(preds + 1e-15) + (1 - y) * np.log(1 - preds + 1e-15))
         ##############################
         return log_l
     
     def update_theta(self, gradient: np.array, lr : float = 0.5):
-        """
-        Function to update the weights in-place.
-
-        Args:
-            gradient: the gradient of the log likelihood.
-            lr: the learning rate.
-
-        Returns:
-            None
-        """
-        ##############################  
+        
+        ##############################
+        #We update model parameters using gradient descent.
         self.parameters += lr * gradient
         ##############################
         pass
         
     @staticmethod
     def compute_gradient(x : np.array, y: np.array, preds: np.array) -> np.array:
-        """
-        Function to compute the gradient of the log likelihood.
-
-        Args:
-            x: it's the input data matrix.
-            y: the label array.
-            preds: the predictions of the input features.
-
-        Returns:
-            gradient: the gradient of the log likelihood.
-        """
+                    gradient: the gradient of the log likelihood.
+       
         ##############################
+        #Then we calculate the gradient for logistic regression parameters
         gradient = np.dot(x.T, (y - preds)) / x.shape[0]
         ##############################
         return gradient
